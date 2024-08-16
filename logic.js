a30db1e9bac93c8c7c8968cb383b3bbd4160520b
Divyansh8843 <divyanshagrawal8843@gmail.com>
2024-08-16 18:28:04 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".pi").forEach(function (el) {
+  new Typed(el, {
+    strings: ["Web Developer", "Competetive Programmer", "Frontend Developer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 100,
+    startDelay: 100,
+    backDelay: 1500,
+  });
+});
